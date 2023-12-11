96c4bf030394a7345e510f4f757a916a05074ffe
alexandre-vidal <alexandre.vidal@laplateforme.io>
2023-12-11 10:09:37 +0100

    Working clock in main.py

--- main.py
+++ main.py
@@ -1,18 +1,57 @@
-from tkinter import *
-from tkinter.ttk import *
+import datetime
+import time
 
-from time import strftime
+def afficher_heure(): 
+    starttime = time.time()
+    while True :
+        from time import strftime
+        if alarm == "oui":
+            if alarm_heure == strftime('%H:%M:%S'):
+                print("Dring Dring")
+        string = strftime('%H:%M:%S')
+        print(string)
+        time.sleep(1.0 - ((time.time() - starttime) % 1.0))
+    
+        
+def alarme(alarm_heure):
+    invalid = True
 
-root = Tk()
-root.title("Horloge")
+    while invalid:
+        userInput = alarm_heure
+        
+        alarmTime = [int(n) for n in userInput.split(":")]
+        
+        
+        if alarmTime[0] >= 24 or alarmTime[0] < 0:
+            print("Votre valeur est incorrecte")
+            alarm_heure = input("Entrez une heure valide pour l'alarme (Ex. 06:30:00) : ")
+            invalid = True
+        elif alarmTime[1] > 60 or alarmTime[1] < 0:
+            print("Votre valeur est incorrecte")
+            alarm_heure = input("Entrez une heure valide pour l'alarme (Ex. 06:30:00) : ")
+            invalid = True
+        else:
+            invalid = False
+            
+    seconds_hms = [3600, 60, 1]
 
-def afficher_heure():
-    string = strftime('%D | %H:%M:%S')
-    label.config(text=string)
-    label.after(1000, afficher_heure)
+    alarmSeconds = sum([a*b for a,b in zip(seconds_hms[:len(alarmTime)], alarmTime)])
 
-label = Label(root, font=("Arial", 80), background = "black", foreground = "white")
-label.pack(anchor='center')
+    now = datetime.datetime.now()
+    currentTimeInSeconds = sum([a*b for a,b in zip(seconds_hms, [now.hour, now.minute, now.second])])
+
+    secondsUntilAlarm = alarmSeconds - currentTimeInSeconds
+
+    if secondsUntilAlarm < 0:
+        secondsUntilAlarm += 86400
+        
+    print("L'alarme est réglée")
+    print("L'alarme sonnera dans %s" % datetime.timedelta(seconds=secondsUntilAlarm))
+            
+print("Voulez-vous une alarme ?")
+alarm = input("oui ou non : ")
+if alarm == "oui":
+    alarm_heure = input("Entrez une heure valide pour l'alarme (Ex. 06:30:00) : ")
+    alarme(alarm_heure)
+
 afficher_heure()
-
-mainloop()
